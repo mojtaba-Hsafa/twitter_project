30b6a60aa101993ab91d61f9aaacbb3f9745b9da
Mojtaba Hsafa <mh4pk@virginia.edu>
2018-09-19 23:19:17 -0400

    first twitter connection

---
+++ twitter_test1.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 19 19:45:16 2018
+
+@author: Moji
+"""
+
+from tweeter_credentials import *
+import twitter
+
+api = twitter.Api(consumer_key=CONSUMER_KEY,
+  consumer_secret= CONSUMER_SECRET,
+  access_token_key= ACCESS_TOKEN,
+  access_token_secret= ACCESS_TOKEN_SECRET)
+
+search = api.GetSearch("ISIS") # Replace happy with your search
+for tweet in search:
+    print(tweet.id, tweet.text)
