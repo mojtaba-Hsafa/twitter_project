59606d6f739dce476ad4cfb612ef1b319eef4a1d
Mojtaba Hsafa <mh4pk@virginia.edu>
2018-10-04 12:59:39 -0400

    tweet text preprocessing script added

---
+++ preprocess-tweet-text.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+From Marco Bonzanini website
+
+@author: Marco Bonzanini
+"""
+
+import re
+ 
+emoticons_str = r"""
+    (?:
+        [:=;] # Eyes
+        [oO\-]? # Nose (optional)
+        [D\)\]\(\]/\\OpP] # Mouth
+    )"""
+ 
+regex_str = [
+    emoticons_str,
+    r'<[^>]+>', # HTML tags
+    r'(?:@[\w_]+)', # @-mentions
+    r"(?:\#+[\w_]+[\w\'_\-]*[\w_]+)", # hash-tags
+    r'http[s]?://(?:[a-z]|[0-9]|[$-_@.&amp;+]|[!*\(\),]|(?:%[0-9a-f][0-9a-f]))+', # URLs
+ 
+    r'(?:(?:\d+,?)+(?:\.?\d+)?)', # numbers
+    r"(?:[a-z][a-z'\-_]+[a-z])", # words with - and '
+    r'(?:[\w_]+)', # other words
+    r'(?:\S)' # anything else
+]
+    
+tokens_re = re.compile(r'('+'|'.join(regex_str)+')', re.VERBOSE | re.IGNORECASE)
+emoticon_re = re.compile(r'^'+emoticons_str+'$', re.VERBOSE | re.IGNORECASE)
+ 
+def tokenize(s):
+    return tokens_re.findall(s)
+
+def preprocess(s, lowercase=False):
+    tokens = tokenize(s)
+    if lowercase:
+        tokens = [token if emoticon_re.search(token) else token.lower() for token in tokens]
+    return tokens
+
+def retweeted(tokenized_tweet):
+    return tokenized_tweet[0] == 'rt'
+processed_tweets=[]
+for a_tweet in trump_text:
+    processed_tweets.append( preprocess(a_tweet, lowercase = True))
+non_retweets_tweet = [tweet for tweet in processed_tweets if retweeted(tweet)==False]
